454f860adce74f2a43799d2f066971125ff7b11a
gerivanc <119078732+gerivanc@users.noreply.github.com>
2025-04-28 10:07:11 -0400

    Update password_generator.py
    Add all possible improvements to the publication with a view to clarifying the project for the user.

--- entropy_password_generator/password_generator.py
+++ entropy_password_generator/password_generator.py
@@ -20,7 +20,7 @@ Author: gerivanc
 GitHub: https://github.com/gerivanc
 MIT License: https://github.com/gerivanc/entropy-password-generator/blob/main/LICENSE.md
 Changelog: https://github.com/gerivanc/entropy-password-generator/blob/main/CHANGELOG.md
-Version: 0.4.0
+Version: 0.4.1
 """
 
 import secrets
@@ -52,7 +52,7 @@ def print_header():
         "GitHub: https://github.com/gerivanc\n"
         "MIT License: https://github.com/gerivanc/entropy-password-generator/blob/main/LICENSE.md\n"
         "Changelog: https://github.com/gerivanc/entropy-password-generator/blob/main/CHANGELOG.md\n"
-        "Version: 0.4.0\n"
+        "Version: 0.4.1\n"
         "----------------------------------------\n"
     )
     print(header)
