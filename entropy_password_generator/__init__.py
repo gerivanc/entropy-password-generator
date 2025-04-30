ed5a728bb30a5004ea7e3219f7c271380c866a5e
gerivanc <119078732+gerivanc@users.noreply.github.com>
2025-04-30 18:58:25 -0400

    Update __init__.py
    Update_2025/04/30:__version__ = "0.4.5"

--- entropy_password_generator/__init__.py
+++ entropy_password_generator/__init__.py
@@ -4,6 +4,6 @@ This package provides a secure password generator with configurable character se
 and entropy calculation, supporting 20 predefined modes.
 """
 
-__version__ = "0.4.4"
+__version__ = "0.4.5"
 from .password_generator import generate_password
 __all__ = ["generate_password"]
