b13e63707b0535aee7827f98e0eee94867696080
Gerivan Costa <119078732+gerivanc@users.noreply.github.com>
2025-05-30 08:17:59 -0400

    Update password_generator.py
    Update password_generator.py

--- entropy_password_generator/password_generator.py
+++ entropy_password_generator/password_generator.py
@@ -27,7 +27,7 @@ Changelog:
 https://github.com/gerivanc/entropy-password-generator/blob/main/CHANGELOG.md
 Issue Report:
 https://github.com/gerivanc/entropy-password-generator/blob/main/.github/ISSUE_TEMPLATE/issue_template.md
-Version: 0.5.8
+Version: 0.5.9
 """
 
 import secrets
@@ -110,7 +110,7 @@ def print_header():
         "generator/blob/main/CHANGELOG.md\n"
         "Issue Report: https://github.com/gerivanc/entropy-password-"
         "generator/blob/main/.github/ISSUE_TEMPLATE/issue_template.md\n"
-        "Version: 0.5.8\n"
+        "Version: 0.5.9\n"
         "----------------------------------------\n"
     )
     print(header)
